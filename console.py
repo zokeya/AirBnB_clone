7016d27f2b99fe25979745f4168f8df963f1dc55
zokeya <zdokeya@gmail.com>
2023-05-23 18:04:08 +0300

    6. Console 0.0.1

--- console.py
+++ console.py
@@ -8,12 +8,16 @@ class HBNBCommand(cmd.Cmd):
   prompt = "(hbnb) "
 
   def do_quit(self, line):
-    "exit the program"
+    "Quit command to exit the program"
     return True
 
   def do_EOF(self, line):
-    "exit the program"
+    "EOF to exit the program"
     return True
+
+  def emptyline(self):
+    "Do nothing with empty line"
+    pass
 
 if __name__ == '__main__':
   HBNBCommand().cmdloop()
